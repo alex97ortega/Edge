0b807f68b1b859bd9435599f6119c9edc742f2fd
alex97ortega <alejor01@ucm.es>
2020-03-01 11:54:42 +0100

    MainMenu como menú para elegir tutorial o comenzar experimento

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour {
     public int numNiveles;
     NivelInfo[] infoLevels;
     uint currentLevel;
+    bool inTutorial; // we will register Events only if we are NOT playing the tutorial
 
     struct NivelInfo
     {
@@ -24,21 +25,29 @@ public class GameManager : MonoBehaviour {
 	}
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+        if (Input.GetKeyDown(KeyCode.Escape)) Quit();
     }
+
+    // levels
     public void LevelPassed(string _time)
     {
         infoLevels[currentLevel-1].levelTime = _time;        
         SceneManager.LoadScene("MenuInfo");
     }
+    public void NextLevel() { currentLevel++; }
+    public uint GetLevel() { return currentLevel; }
+    public void StartTutorial()   { currentLevel = 1; inTutorial = true;  SceneManager.LoadScene("Nivel1");  }
+    public void StartExperiment() { currentLevel = 4; inTutorial = false; SceneManager.LoadScene("Nivel4");  }
+
+    // items
     public void ItemGotten() {
         infoLevels[currentLevel-1].items++;
     }
     public void SetTotalItems(uint totalItems) { infoLevels[currentLevel - 1].totalItems = totalItems; }
-    public void NextLevel() { currentLevel++; }
+
+    // player Deaths
     public void Dead() { infoLevels[currentLevel - 1].deaths++; }
 
-    public uint GetLevel() { return currentLevel; }
     // for level info
     public string GetLevelTime() { return infoLevels[currentLevel - 1].levelTime; }
     public string GetLevelDeaths() { return infoLevels[currentLevel - 1].deaths.ToString(); }
@@ -46,5 +55,11 @@ public class GameManager : MonoBehaviour {
         string text;
         text = infoLevels[currentLevel - 1].items.ToString() + "/" + infoLevels[currentLevel - 1].totalItems.ToString();
         return text;
+    }
+
+    // exit
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ Assets/Scripts/Managers/SessionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// clase que registra los parámetros del juego que necesitaremos para hacer el análisis
+// se va llamando desde el GameManager cuando ocurre un evento notable en el juego 
+public class SessionManager : MonoBehaviour {
+
+    private int user_id;
+    private float count_down;
+    //private API api;
+
+	// Use this for initialization
+	void Start () {
+		//api = new API();
+	}
+
+    // llamadas públicas para llamar desde el GameManager
+    public void LogStartLevel()
+    {
+
+    }
+    public void LogEndLevel()
+    {
+
+    }
+    public void LogItemGotten()
+    {
+
+    }
+    public void LogPlayerDead()
+    {
+
+    }
+    
+    // método privado para registrar parámetros que sean iguales para todos los eventos
+	private void LogEvent()
+    {
+
+    }
+}
